89fcd6203bc1650c1bc6abd2a5268944402aac8e
admin <ovchinnikov_petr@mail.ru>
2022-09-06 22:48:19 +0300

    Initial commit

---
+++ src/main/java/com/example/FarmMarket/Controllers/FirstController.java
@@ -0,0 +1,13 @@
+package com.example.FarmMarket.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/")
+    public String getFirstPage() {
+        return "home";
+    }
+}
---
+++ src/main/java/com/example/FarmMarket/FarmMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.FarmMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmMarketApplication.class, args);
+	}
+
+}
