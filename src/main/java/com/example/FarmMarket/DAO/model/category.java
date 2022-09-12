ebfaa5268ff9b1d39365917a97a690b1ff1d8aa4
admin <ovchinnikov_petr@mail.ru>
2022-09-12 18:45:20 +0300

    html

---
+++ src/main/java/com/example/FarmMarket/DAO/model/category.java
@@ -0,0 +1,4 @@
+package com.example.FarmMarket.DAO.model;
+
+public class category {
+}
--- src/main/java/com/example/FarmMarket/web/controller/FirstController.java
+++ src/main/java/com/example/FarmMarket/web/controller/FirstController.java
@@ -1,4 +1,4 @@
-package com.example.FarmMarket.Controllers;
+package com.example.FarmMarket.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
